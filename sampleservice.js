6b97bd1a207f3238c2613b4a5f8329e4c6d9dcd8
40004738 <indrani.k@Ltts.com>
2019-08-23 17:50:12 +0530

    updated dynamic dropdowns in add & edit account by indrani 23-8-2019 17:50

--- sampleservice.js
+++ sampleservice.js
@@ -772,3 +772,61 @@ exports.Contactdep = function (req, res) {
     });
 
 };
+exports.accountcat = function (req, res) {
+
+    let query1="call procLookUpAccountCategories()"; 
+   
+    db.query(query1, (err, result) => {
+   if (err) {
+     
+       return res.status(500).send(err);
+   }
+   res.status(200).json(result);
+          
+});
+
+};
+exports.accountvert = function (req, res) {
+
+ let query1="call procLookUpVertical()"; 
+
+ db.query(query1, (err, result) => {
+if (err) {
+  
+    return res.status(500).send(err);
+}
+res.status(200).json(result);
+       
+});
+
+};
+exports.accountcont = function (req, res) {
+
+ let query1="call procLookUpCountries()"; 
+
+ db.query(query1, (err, result) => {
+if (err) {
+  
+    return res.status(500).send(err);
+}
+res.status(200).json(result);
+       
+});
+
+};
+exports.accountstate = function (req, res) {
+
+ let iCountry= req.params.country;
+//console.log(iCountryID)
+ let query1='call procLookUpStates("' + iCountry+ '")' ;
+
+ db.query(query1, (err, result) => {
+if (err) {
+  
+    return res.status(500).send(err);
+}
+res.status(200).json(result);
+
+});
+
+};
