1266b92b337708485d8cc3ff27074eb298cde67e
Arpit Bhaderia <Arpit.bhaderia@Ltts.com>
2019-08-23 15:36:55 +0530

    Removing all files

--- systemserviceindex.js
+++
@@ -1,8 +0,0 @@
-const app = require('cirruswave');
-const initoptions={
- "aclconfig":"aclconfig.json",
- "secret":"helloworld",
- "appserviceconfig":"appserviceconfig.json",
- "appdir":__dirname
-} 
-app.startsystemservice("keymgr",initoptions);
